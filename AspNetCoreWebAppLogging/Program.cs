0ae44325bbd6ffd110f3b57b071a96c25b3ad8b5
EricMoritz <eric.moritz@optelgroup.com>
2018-12-11 10:34:59 -0600

    Intial check in.

---
+++ AspNetCoreWebAppLogging/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.IO;
+
+namespace AspNetCoreWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config =
+                new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .Build();
+
+            // This line uses Serilog.Settings.Configuration
+            var configuration =
+                new LoggerConfiguration()
+                    .ReadFrom.Configuration(config);
+
+            // This allows you to use the PushProperty() functionality in Serilog.
+            // For more details: https://github.com/serilog/serilog/wiki/Enrichment
+            configuration.Enrich.FromLogContext();
+
+            Log.Logger = configuration.CreateLogger();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                   .UseSerilog();
+    }
+}
---
+++ Net4.6FrameworkLogging/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Serilog;
+using Serilog.Context;
+using System.Diagnostics;
+using Serilog.Events;
+
+namespace Net46ConsoleApp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Log.Logger =
+                new LoggerConfiguration()
+                    .ReadFrom.AppSettings("file")
+                    .ReadFrom.AppSettings("console")
+                    .MinimumLevel.Is(LogEventLevel.Verbose)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+
+            using (LogContext.PushProperty("ProcessName", Process.GetCurrentProcess().ProcessName))
+            {
+                Log.Verbose("Verbose log entry.");
+                Log.Debug("Debug log entry.");
+                Log.Information("Information log entry.");
+                Log.Warning("Warning log entry.");
+                Log.Error("Error log entry.");
+                Log.Fatal("Fatal log entry.");
+            }
+
+            Console.WriteLine("Any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
